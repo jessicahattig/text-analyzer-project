91ce37aac8c640c02eb6587b2ed56076070ae671
jessicahattig <jessicahattig@gmail.com>
2023-06-21 19:15:20 -0700

    Add jest test to numberOccurrencesInText.

--- README.md
+++ README.md
@@ -73,10 +73,17 @@ const word = "Red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 3
 
-Describe(removeOffensiveWordsInText)
+<!-- Describe(removeOffensiveWordsInText)
   Test: It should return text without the word 'zoink'.
   Code:
   const text = "omg, i stubbed my toe, zoink!"
   const word = "zoink";
   removeOffensiveWordsInText (word, text)
-  Expected Output: "omg, i stubbed my toe,!"
+  Expected Output: "omg, i stubbed my toe,!" -->
+
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+numberOfOccurrencesInText(word, text);
+Expected Output: 0
--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,9 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
+  if (word.trim().length === 0) {
+    return 0;
+  }
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function(element) {
@@ -25,8 +28,22 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
+
 // function removeOffensiveWordsInText(word, text) {
 //   const textArray = text.replace (' '));
 //   textArray.forEach(function(element('zoinK', ' ')
 
 // UI Logic
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
